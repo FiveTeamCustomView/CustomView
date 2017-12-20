da2a0aa67ffc1cfa4a24399aeaf8107fa51468a2
huxiangxu499 <107370394@qq.com>
2017-12-20 10:22:41 +0800

    view

---
+++ CustomView/app/src/main/java/com/example/administrator/customview/MyView.java
@@ -0,0 +1,68 @@
+package com.example.administrator.customview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by lenovo on 2017/12/19.
+ */
+
+public class MyView extends View {
+
+    private Point controlPoint = new Point(200, 200);
+
+    public MyView(Context context) {
+        super(context);
+    }
+
+    public MyView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.BLACK);
+        paint.setStrokeWidth(10);
+
+        Path path = new Path();
+        path.moveTo(100, 500);
+        path.quadTo(controlPoint.x, controlPoint.y, 700, 500);
+        //绘制路径
+        canvas.drawPath(path, paint);
+        //绘制辅助点
+        canvas.drawPoint(controlPoint.x,controlPoint.y,paint);
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+                controlPoint.x = (int) event.getX();
+                controlPoint.y = (int) event.getY();
+                invalidate();
+                break;
+        }
+        return true;
+    }
+}
--- CustomView/app/src/main/java/com/example/administrator/customview/activity/HuActivity.java
+++ CustomView/app/src/main/java/com/example/administrator/customview/activity/HuActivity.java
@@ -2,14 +2,55 @@ package com.example.administrator.customview.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.administrator.customview.LikeStar;
+import com.example.administrator.customview.MyView1;
+import com.example.administrator.customview.MyViewCircle;
 import com.example.administrator.customview.R;
 
 public class HuActivity extends AppCompatActivity {
 
+    private MyView1 myview;
+    private Button btn;
+    private MyViewCircle myViewCircle;
+    private LikeStar mLikeStar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hu);
+        //三阶
+//        myview = (MyView1) findViewById(R.id.myview);
+//        btn = (Button) findViewById(R.id.btn);
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                myview.setControlPointTwo(!myview.isControlPointTwo());
+//            }
+//        });
+//
+//        //圆变心
+//        myViewCircle = (MyViewCircle) findViewById(R.id.myview);
+//
+//        btn = (Button) findViewById(R.id.btn);
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                myViewCircle.setRuning(true);
+//            }
+//        });
+
+
+//点赞效果
+        mLikeStar = (LikeStar) findViewById(R.id.likestart);
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mLikeStar.startRunning();
+            }
+        });
     }
+
 }
